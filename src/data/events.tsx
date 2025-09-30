50e6b5c6c98355d82eaab90635c06ac12406a12f
Satoshi Matsuzawa <16040628+chacco38@users.noreply.github.com>
2025-09-30 18:10:56 +0900

    Update events.tsx

--- src/data/events.tsx
+++ src/data/events.tsx
@@ -10,13 +10,13 @@ export type EventItem = {
 const Events: EventItem[] = [
   {
     title: 'Japan FinOps Meetup#5',
-    date: "次回: 2025年10月16日",
+    date: "次回: 2025年10月16日(木)",
     link: '    link: 'https://finops.connpass.com/event/368385/',
     image: 'https://media.connpass.com/thumbs/3d/7d/3d7da2ba261173b058991c10b7e43a2b.png',
   },
   {
     title: 'LF Japan Community Days大阪 feat. FinOps',
-    date: "次回: 2025年10月21～22日",
+    date: "次回: 2025年10月21日(火)-22日(水)",
     link: 'https://finops.connpass.com/event/368384/',
     image: 'https://media.connpass.com/thumbs/3d/7d/3d7da2ba261173b058991c10b7e43a2b.png',
   },
